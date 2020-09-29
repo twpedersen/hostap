50de69048c0133370476fd6d543c8a63030e91e0
Thomas Pedersen <thomas@adapt-ip.com>
2020-09-29 11:49:32 -0700

    tests: add ap_max_inactivity test for S1G
    The listen interval encoding changes for S1G, so ensure it
    still works.
    
    Signed-off-by: Thomas Pedersen <thomas@adapt-ip.com>

--- tests/hwsim/test_s1g.py
+++ tests/hwsim/test_s1g.py
@@ -86,6 +86,7 @@ def test_s1g_open_1in2_scan(dev, apdev):
             raise Exception("STA associated on wrong operating frequency? " + str(sig))
     finally:
         set_world_reg(apdev[0], None, dev[0])
+
 def test_s1g_open_1in2(dev, apdev):
     """S1G with 1MHz primary inside 2MHz operating"""
     hapd = None
@@ -103,5 +104,45 @@ def test_s1g_open_1in2(dev, apdev):
             raise Exception("STA associated on wrong frequency? " + str(sig))
         if "CENTER_FRQ1=911" not in sig:
             raise Exception("STA associated on wrong operating frequency? " + str(sig))
+    finally:
+        set_world_reg(apdev[0], None, dev[0])
+
+def test_s1g_bss_keep_alive(dev, apdev):
+    """S1G WNM keep-alive"""
+    hapd = None
+    params = { "ssid": "s1g",
+               "hw_mode": "ah",
+               "channel": "1",
+               "country_code": "US",
+               "ap_max_inactivity": "1"}
+    try:
+        hapd = hostapd.add_ap(apdev[0], params)
+        dev[0].connect("s1g", key_mgmt="NONE", scan_freq="902.5")
+
+        addr = dev[0].own_addr()
+
+        start = hapd.get_sta(addr)
+        ev = dev[0].wait_event(["CTRL-EVENT-DISCONNECTED"], timeout=2)
+        if ev is not None:
+            raise Exception("Unexpected disconnection")
+        end = hapd.get_sta(addr)
+        if int(end['rx_packets']) <= int(start['rx_packets']):
+            raise Exception("No keep-alive packets received")
+        try:
+            # Disable client keep-alive so that hostapd will verify connection
+            # with client poll
+            dev[0].request("SET no_keep_alive 1")
+            for i in range(60):
+                sta = hapd.get_sta(addr)
+                logger.info("timeout_next=%s rx_packets=%s tx_packets=%s" % (sta['timeout_next'], sta['rx_packets'], sta['tx_packets']))
+                if i > 1 and sta['timeout_next'] != "NULLFUNC POLL" and int(sta['tx_packets']) > int(end['tx_packets']):
+                    break
+                ev = dev[0].wait_event(["CTRL-EVENT-DISCONNECTED"], timeout=0.5)
+                if ev is not None:
+                    raise Exception("Unexpected disconnection (client poll expected)")
+        finally:
+            dev[0].request("SET no_keep_alive 0")
+        if int(sta['tx_packets']) <= int(end['tx_packets']):
+            raise Exception("No client poll packet seen")
     finally:
         set_world_reg(apdev[0], None, dev[0])
